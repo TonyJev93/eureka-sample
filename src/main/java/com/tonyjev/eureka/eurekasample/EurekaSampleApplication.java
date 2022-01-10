10f66eea9981e2e7707e84c5aa6926b75be38dc8
JUN GYEWON <tonyjev93@gmail.com>
2022-01-10 15:39:06 +0900

    chore: eureka 기본 환경설정 추가

--- src/main/java/com/tonyjev/eureka/eurekasample/EurekaSampleApplication.java
+++ src/main/java/com/tonyjev/eureka/eurekasample/EurekaSampleApplication.java
@@ -2,8 +2,10 @@ package com.tonyjev.eureka.eurekasample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // Eureka 서버로서 동작하도록 함
 public class EurekaSampleApplication {
 
     public static void main(String[] args) {
