8850dba9b5fe77bf1e76b04df2c225ba63cadeff
JUN GYEWON <tonyjev93@gmail.com>
2022-01-10 15:33:33 +0900

    init

---
+++ src/main/java/com/tonyjev/eureka/eurekasample/EurekaSampleApplication.java
@@ -0,0 +1,13 @@
+package com.tonyjev.eureka.eurekasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tonyjev/eureka/eurekasample/EurekaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonyjev.eureka.eurekasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
